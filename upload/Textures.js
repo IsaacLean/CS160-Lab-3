7df5e4a65c32c7e5977c4eb8beb7ff48f5242a66
IsaacLean <isaaclean92@gmail.com>
2014-11-25 23:27:11 -0800

    Got texture switching working

--- upload/Textures.js
+++ upload/Textures.js
@@ -129,6 +129,24 @@ window.onload = function init()
             Render(texture0, texture1);
         }, 16
     );
+	
+	document.getElementById("btn_earthTexture").onclick = function(){
+        texture0 = CreateTexture('earth.jpg',
+        function(texture, image)
+        {
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        }
+		)
+    };
+	
+	document.getElementById("btn_nullTexture").onclick = function(){
+        texture0 = null;
+    };
 };
 
 function CreateTexture(file, loaded) 
