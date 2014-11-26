375325eeef4eda8de5e427a7091b174a500279c4
IsaacLean <isaaclean@creatibyte.com>
2014-11-25 23:45:07 -0800

    Added new textures

--- upload/Textures.js
+++ upload/Textures.js
@@ -144,6 +144,34 @@ window.onload = function init()
 		)
     };
 	
+	document.getElementById("btn_checkerboardTexture").onclick = function(){
+        texture0 = CreateTexture('cbtexture.png',
+        function(texture, image)
+        {
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        }
+		)
+    };
+	
+	document.getElementById("btn_lavaTexture").onclick = function(){
+        texture0 = CreateTexture('lava.jpg',
+        function(texture, image)
+        {
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        }
+		)
+    };
+	
 	document.getElementById("btn_nullTexture").onclick = function(){
         texture0 = null;
     };
